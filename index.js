d5e344a7fb7f24186fdad55937aeea1a29125947
Humberto Ham <hamduranhumberto@gmail.com>
2022-02-07 11:27:36 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/typeDef");
+const { MONGODB } = require("./config");
+
+const PORT = process.env.port || 4000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("mongodb connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at port ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
